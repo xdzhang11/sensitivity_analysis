bfb26ca4ac0e32328c3aadc96e16ec7966bdfbbd
Xiaodong Zhang <xiazang@dtu.dk>
2022-12-10 21:15:24 +0100

    minor changes

--- main_cost.py
+++ main_cost.py
@@ -8,20 +8,20 @@ filename_Xy = os.path.join('Xy', 'Xy.h5')
 df = pd.read_hdf(filename_Xy, 'Xy')
 df.head()
 #%%
-X = df.iloc[:,0:5]
+X = df.iloc[:, 0:5]
 X = X.copy()
 X.cl = 0.7+(1.3-0.7)*X.cl
-X.bladeIx  = 0.7+(1.3-0.7)*X.bladeIx
-X.towerIx  = 0.7+(1.3-0.7)*X.towerIx
+X.bladeIx = 0.7+(1.3-0.7)*X.bladeIx
+X.towerIx = 0.7+(1.3-0.7)*X.towerIx
 
 #%%
-u = 3+X.loc[:,'wsp']*(27-3)
+u = 3+X.loc[:, 'wsp']*(27-3)
 sigma_min = np.maximum(0, 0.1*(u-20))
 sigma_max = 0.18*(6.8+0.75*u)
 sigma = sigma_min+(sigma_max-sigma_min)*X.loc[:,'ti']
 ti = sigma/u
-X.loc[:,'wsp'] = u
-X.loc[:,'ti'] = ti
+X.loc[:, 'wsp'] = u
+X.loc[:, 'ti'] = ti
 X.head()
 
 #%%
