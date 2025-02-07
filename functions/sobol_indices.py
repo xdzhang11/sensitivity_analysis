cdcd04ac31e635199f15c120f2308e774928f4bc
Xiaodong Zhang <xdotzhang@hotmail.com>
2025-02-07 09:48:11 +0800

    added sobol_analysis function

--- functions/sobol_indices.py
+++ functions/sobol_indices.py
@@ -1,11 +1,25 @@
 import numpy as np
 import copy
-from sample_generation import x_all
+import joblib
+import time
 
-def sobol(cost, d, n):
-    X_A = x_all(n)
-    X_B = x_all(n)
-    X_R = x_all(n)
+#%%
+def sobol(cost, d, n, sample_inputs):
+    """
+    Compute Sobol sensitivity indices.
+
+    Args:
+        cost (function): Cost function to evaluate.
+        d (int): Number of input dimensions.
+        n (int): Number of samples.
+        sample_inputs (function): Function to sample input variables.
+
+    Returns:
+        tuple: (First-order indices, total-order indices)
+    """
+    X_A = sample_inputs(n)
+    X_B = sample_inputs(n)
+    X_R = sample_inputs(n)
 
     y_A = cost(X_A).reshape((n, 1))
     y_B = cost(X_B).reshape((1, n))
@@ -23,3 +37,33 @@ def sobol(cost, d, n):
         S_Ti[i] = np.sum((y_A - y_A_Bi) ** 2) / 2 / n / varY
 
     return S_i, S_Ti
+
+
+#%%
+def sobol_analysis(model_path, n, d, sample_inputs):
+    """
+    Perform Sobol sensitivity analysis using the Sobol indices method.
+
+    Args:
+        model_path (str): Path to the saved model (joblib file).
+        n (int): Number of samples for analysis.
+        d (int): Number of input dimensions.
+        sample_inputs (function): Function to sample input variables.
+
+    Returns:
+        dict: Sobol indices, elapsed time, and sample size.
+    """
+    # Load the model
+    cb_rg = joblib.load(model_path)
+
+    # Cost function for predictions
+    def cost(x):
+        return cb_rg.predict(x)
+
+    # Measure time
+    t_start = time.time()
+    S_i, S_Ti = sobol(cost, d, n, sample_inputs)
+    elapsed_time = time.time() - t_start
+
+    # Return results as a dictionary
+    return {'Si': S_i.tolist(), 'STi': S_Ti.tolist(), 'Time': elapsed_time, 'n': n}
